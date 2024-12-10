5d7a70956cf91ba4957a6ac75fcdab45c867b3d7
Shreyas4603 <shreyasthegreat7@gmail.com>
2024-12-10 16:28:58 +0530

    updtaed cors

--- src/main/java/com/example/Compiler/Config/WebConfig.java
+++ src/main/java/com/example/Compiler/Config/WebConfig.java
@@ -13,10 +13,24 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // Allows any origin, compatible with credentials
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://13.234.29.166:8080",
+                                "https://13.234.29.166:8080",
+                                "https://ec2-13-234-29-166.ap-south-1.compute.amazonaws.com",
+                                "http://ec2-13-234-29-166.ap-south-1.compute.amazonaws.com",
+                                "http://65.2.126.202:9000",
+                                "https://65.2.126.202:9000",
+                                "https://ec2-65-2-126-202.ap-south-1.compute.amazonaws.com",
+                                "http://ec2-65-2-126-202.ap-south-1.compute.amazonaws.com",
+                                "http://15.206.209.169:8000",
+                                "https://15.206.209.169:8000",
+                                "https://ec2-15-206-209-169.ap-south-1.compute.amazonaws.com",
+                                "http://ec2-15-206-209-169.ap-south-1.compute.amazonaws.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // Required if you need cookies or authorization headers
+                        .allowCredentials(true);
             }
         };
     }
